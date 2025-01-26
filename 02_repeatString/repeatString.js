4ef8f07ed89aa680687f07f59ba3c668ff6e9a30
eldude <eldude73@gmail.com>
2025-01-26 21:59:51 +1100

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeat = "";
+    for (let i=num; i>0; i--) {
+        repeat += string;
+    }
+    return repeat;
 };
+
+repeatString("hello", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
