d162c9b9a4eb156a04bd9d4470a134a64cf3b288
eldude <eldude73@gmail.com>
2025-01-28 00:52:10 +1100

    finished exercide 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
 
+    if (startNum <= 0 || endNum <= 0 || !Number.isInteger(startNum) 
+        || !Number.isInteger(endNum) || Number.isNaN(endNum) || Number.isNaN(startNum)) {
+        return "ERROR";
+    }
+
+    if (startNum > endNum) {
+        start = endNum;
+        end = startNum;
+    } else {
+        start = startNum;
+        end = endNum;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
 };
 
+console.log(sumAll(1, "h"));
 // Do not edit below this line
 module.exports = sumAll;
