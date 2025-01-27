0e84882463011f688f477fff47c7f79e87f39c21
eldude <eldude73@gmail.com>
2025-01-27 23:47:04 +1100

    this time I actually completed exercide 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...elements) {
+    
+    for (let element of elements) {
+        let index = array.indexOf(element);
+        while (index > -1) { // only splice array when item is found
+            array.splice(index, 1); // 2nd parameter means remove one item only
+            index = array.indexOf(element);
+        }
+    }
+    return array;
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
