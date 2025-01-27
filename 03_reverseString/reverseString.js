a94bad3bc971673039c9060c540bb5160b9d6889
eldude <eldude73@gmail.com>
2025-01-27 19:40:48 +1100

    exercise 4 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (sentence) {
+    let backward = sentence.split("");;
+    backward = backward.reverse();
+    join = backward.join("");
+    
+    return join;
 
 };
+console.log(reverseString("hello"));
+
 
 // Do not edit below this line
 module.exports = reverseString;
