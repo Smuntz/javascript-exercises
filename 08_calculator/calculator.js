59f1d6d7be398bcd585119c9ad2881a92563114f
eldude <eldude73@gmail.com>
2025-01-29 22:20:07 +1100

    exercise 08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  const totalSum = numbers.reduce((accumulator, item) => {
+    return accumulator += item;
+  }, 0)
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  const totalSum = numbers.reduce((accumulator, item) => {
+    return accumulator = accumulator * item;
+  }, 1)
+  return totalSum;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0)
+    return -1;
+  else if (num == 0)
+    return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
+
 };
 
 // Do not edit below this line
